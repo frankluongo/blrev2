8480f1b69d4aafaa34ad983ea02ef80a1696da12
frankluongo <frank@frankluongo.com>
2020-05-21 21:05:32 -0400

    Lazy Load iFrames

--- frontend/components/cashOffer.php
+++ frontend/components/cashOffer.php
@@ -3,7 +3,8 @@
     <h3><?php the_field('theme_options_podio_form_title', 'option'); ?></h3>
   </div>
   <iframe
-    src="https://podio.com/webforms/<?php echo get_field('theme_options_podio_app_id', 'option'); ?>/<?php echo get_field('theme_options_podio_form_id', 'option'); ?>?e=true"
+    data-lazy-iframe
+    data-src="https://podio.com/webforms/<?php echo get_field('theme_options_podio_app_id', 'option'); ?>/<?php echo get_field('theme_options_podio_form_id', 'option'); ?>?e=true"
     title="Cash Offer Webform"
   ></iframe>
 </div>
