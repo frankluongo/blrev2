4e9a04c984fda97d225893e780134506b5b3a0ac
frankluongo <frank@frankluongo.com>
2020-06-04 17:28:04 -0400

    Add Shortcode for Checkbox Item

---
+++ src/templates/components/checkboxItem.php
@@ -0,0 +1,8 @@
+<section class="checkbox-item">
+  <div class="checkbox-item__svg">
+    <?php get_template_part('svgs/checkmark'); ?>
+  </div>
+  <div class="checkbox-item__text">
+    <?php echo $text; ?>
+  </div>
+</section>
--- src/templates/includes/Shortcodes.php
+++ src/templates/includes/Shortcodes.php
@@ -1,5 +1,15 @@
 <?php
-function pageTitleFn(){
-	return get_the_title();;
-}
-add_shortcode( 'pageTitle', 'pageTitleFn' );
+
+  function pageTitleFn() {
+    return get_the_title();
+  };
+
+  function checkmarkItemFn($atts) {
+    $text = $atts['text'];
+    ob_start();
+    include(locate_template('components/checkboxItem.php'));
+    return ob_get_clean();
+  };
+
+  add_shortcode( 'checkmarkItem', 'checkmarkItemFn');
+  add_shortcode( 'pageTitle', 'pageTitleFn' );
