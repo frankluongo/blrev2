6965e5fca7ca7b183b08bafb9806cb2f4c80e823
frankluongo <frank@frankluongo.com>
2020-06-09 20:26:38 -0400

    Update Text Styling

--- src/templates/components/textImageBlock.php
+++ src/templates/components/textImageBlock.php
@@ -1,5 +1,5 @@
 <section class="block block--bg-<?php echo $block['bgColor']; ?> block--text-<?php echo $block['textColor']; ?>">
-  <div class="container">
+  <div class="container content">
   <div class="text-image text-image--<?php echo $block['imagePosition']; ?>">
     <div class="text-image__image text-image-image--<?php echo $block['columnWidthImage'] ?>">
       <img class="text-image__image-element" data-lazy-img data-src="<?php echo $block['image']['sizes']['medium']; ?>" alt="Brotherly Love Real Estate">
